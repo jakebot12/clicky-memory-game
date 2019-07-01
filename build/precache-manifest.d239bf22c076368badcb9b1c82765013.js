6c46ec1974321921ab61a0de94ed751458946ec9
Jake Gurley <jakemg12@gmail.com>
2019-06-30 21:29:29 -0700

    <Initial Submission - More to come>

---
+++ build/precache-manifest.d239bf22c076368badcb9b1c82765013.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c5f617dae1fe5decffceab075aa984c",
+    "url": "/clicky-memory-game/index.html"
+  },
+  {
+    "revision": "ca6caee2ad9e3334bb03",
+    "url": "/clicky-memory-game/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/clicky-memory-game/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "ca6caee2ad9e3334bb03",
+    "url": "/clicky-memory-game/static/js/main.58853992.chunk.js"
+  },
+  {
+    "revision": "2d83433aa8872a544556",
+    "url": "/clicky-memory-game/static/js/runtime~main.83671481.js"
+  }
+]);
